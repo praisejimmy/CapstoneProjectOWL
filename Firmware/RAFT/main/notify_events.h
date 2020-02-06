42054870386026e675ce0aa706465b7321a0fb4d
Jake Loveland <jakeloveland@gmail.com>
2020-02-05 16:33:35 -0800

    Added H file for events

---
+++ Firmware/RAFT/main/notify_events.h
@@ -0,0 +1,22 @@
+
+#ifndef NOTIFY_EVENTS_H
+#define NOTIFY_EVENTS_H
+
+#define LORA_RECEIVED    0x00000001         //LoRa Message Received
+#define WIFI_RECEIVED    0x00000002         //WiFi Message Received
+#define GPS_RECEIVED     0x00000004
+#define ACK_RECEIVED     0x00000008
+
+#define LORA_READY_SEND  0x00000010        //LoRa Message ReadySend
+#define LORA_RECEIVED_HW 0x00000020
+#define LORA_SENT        0x00000080
+
+#define WIFI_READY_SEND  0x00000100
+#define WIFI_RECEIVED_HW 0x00000200
+#define WIFI_SENT        0x00000800
+
+#define GPS_REQUEST      0x00001000
+#define GPS_RECEIVED_HW  0x00002000
+
+
+#endif
